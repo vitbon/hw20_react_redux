b879abc395ab21b1275dce8dcfcd8252ef4170d2
Vitalii <vit.bondarchuk@gmail.com>
2020-09-30 17:15:46 +0300

    HW20 final

--- build/precache-manifest.10f4f98da3c7fd1204d5be304fdec15e.js
+++ build/precache-manifest.10f4f98da3c7fd1204d5be304fdec15e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "803d478fb41f17daeb26ce2e129e4be7",
+    "revision": "f2902d8fe959ed91a61feb527d342a25",
     "url": "/hw20_react_redux/index.html"
   },
   {
-    "revision": "1d431aba912547fba725",
+    "revision": "b831769e82eb00efcfa0",
     "url": "/hw20_react_redux/static/css/main.a60229a5.chunk.css"
   },
   {
-    "revision": "1822b8017e8c35086b86",
-    "url": "/hw20_react_redux/static/js/2.080de13b.chunk.js"
+    "revision": "c413ce14b536b21584e8",
+    "url": "/hw20_react_redux/static/js/2.81fe0cdf.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/hw20_react_redux/static/js/2.080de13b.chunk.js.LICENSE.txt"
+    "url": "/hw20_react_redux/static/js/2.81fe0cdf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1d431aba912547fba725",
-    "url": "/hw20_react_redux/static/js/main.737ace56.chunk.js"
+    "revision": "b831769e82eb00efcfa0",
+    "url": "/hw20_react_redux/static/js/main.fd4de25c.chunk.js"
   },
   {
     "revision": "02f2875521eb2be91f2c",
